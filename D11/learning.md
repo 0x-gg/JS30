d3426e0addfbac86fa16437ff2d777bfe1ad6059
Gaurav Gada <gauravgada@gmail.com>
2017-03-17 15:35:26 -0700

    D11
    Video player implementation

---
+++ D11/learning.md
@@ -0,0 +1,15 @@
+this.dataset.skip - data-skip attribute
+
+parseFloat(someString); // to convert to number
+
+requestFullScreen();
+cancelFullScreen();
+
+video event listeners:
+play
+pause
+timeupdate/progress - video emits this event, can be used to track progress bar
+
+video.paused
+video.currentTime
+video.duration
---
+++ D11/scripts.js
@@ -0,0 +1,78 @@
+//TODO
+//fullscreen
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const toggleFS = player.querySelector('.fullscreen');
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeupdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = video.currentTime / video.duration * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = e.offsetX / progress.offsetWidth * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      player.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      player.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach((range) => {
+  range.addEventListener('change', handleRangeupdate);
+  range.addEventListener('mousemove', handleRangeupdate);
+});
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+toggleFS.addEventListener('click', toggleFullScreen);
